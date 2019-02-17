fa1bf752fb9ab282ec38659d3afe3f678c7c5b65
kkeshob <kkeshob@users.noreply.github.com>
2019-02-17 09:00:01 +0530

    Add files via upload
    Enter How many Items:
    
    7
    Enter the Weight of item no- 0 :
    
    2
    Enter the Profit of item no- 0 :
    
    10
    Enter the Weight of item no- 1 :
    
    3
    Enter the Profit of item no- 1 :
    
    5
    Enter the Weight of item no- 2 :
    
    5
    Enter the Profit of item no- 2 :
    
    15
    Enter the Weight of item no- 3 :
    
    7
    Enter the Profit of item no- 3 :
    
    7
    Enter the Weight of item no- 4 :
    
    1
    Enter the Profit of item no- 4 :
    
    6
    Enter the Weight of item no- 5 :
    
    4
    Enter the Profit of item no- 5 :
    
    18
    Enter the Weight of item no- 6 :
    
    1
    Enter the Profit of item no- 6 :
    
    3
    Enter the height Sac Size:
    
    15
    Weight    Profit    Pi/Wi        Xi
    ____________________________________
       2        10        5.0        1
       3        5        1.67        0.67
       5        15        3.0        1
       7        7        1.0        0
       1        6        6.0        1
       4        18        4.5        1
       1        3        3.0        1
    Hence Maximum Profit= 55.33333333333333

---
+++ knapsack.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 06:33:16 2019
+
+@author: keshob
+"""
+def knapsac(pr,wg,sac):
+    pdw=[]
+    n=len(pr)
+    item=[]
+    i=0
+    while i<n:
+        pdw.append(pr[i]/wg[i])
+        item.append(0)
+        i=i+1
+    temp=pdw.copy()
+    while sac>0:
+        itemno=temp.index(max(temp))        
+        if wg[itemno]<=sac:
+            item[itemno]=1
+            sac=sac-wg[itemno]
+        else:
+            item[itemno]=sac/wg[itemno]
+            sac=sac-item[itemno]*wg[itemno]
+        temp[itemno]=0
+    mp=0
+    for i in range(0,n):
+        mp=mp+pr[i]*item[i]    
+    print("Weight    Profit    Pi/Wi        Xi")
+    print("____________________________________")
+    for i in range(0,n):
+        print("  ",wg[i],"      ",pr[i],"      ",round(pdw[i],2),"      ",round(item[i],2))
+    print("Hence Maximum Profit=",mp)
+
+print("Enter How many Items:")
+n=int(input())
+profit=[]
+weight=[]
+for i in range(0,n):
+    print("Enter the Weight of item no-",i,":")
+    weight.append(int(input()))
+    print("Enter the Profit of item no-",i,":")
+    profit.append(int(input()))
+print("Enter the height Sac Size:")
+s=int(input())
+knapsac(profit,weight,s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
